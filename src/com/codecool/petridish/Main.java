dedbf46dcf6e052bc31e1ec44b1cc59a2261cbe8
TudorDan <tudor.ist@gmail.com>
2020-08-24 19:36:54 +0300

    Add PetriDish singleton

--- src/com/codecool/petridish/Main.java
+++ src/com/codecool/petridish/Main.java
@@ -2,6 +2,8 @@ package com.codecool.petridish;
 
 public class Main {
     public static void main(String[] args) {
-
+        System.out.println("Starting Project simulation..");
+        PetriDish petriDish = PetriDish.getInstance();
+        System.out.println("End of Project simulation.");
     }
 }
---
+++ src/com/codecool/petridish/PetriDish.java
@@ -0,0 +1,61 @@
+package com.codecool.petridish;
+
+import com.codecool.petridish.LifeForms.Bacillus;
+import com.codecool.petridish.LifeForms.Bacteria;
+import com.codecool.petridish.LifeForms.Coccus;
+import com.codecool.petridish.Utilities.BacteriaType;
+import com.codecool.petridish.Utilities.Position;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class PetriDish {
+    private static PetriDish instance = null;
+    private static List<Bacteria> bacteriaList = new ArrayList<>();
+    private static int timePulse;
+    private int width;
+
+    private PetriDish() {
+    }
+
+    public static PetriDish getInstance() {
+        if (instance == null) {
+            instance = new PetriDish();
+            System.out.println("[Global] Petri dish created.");
+        }
+        return instance;
+    }
+
+    public static void setBacteriaList() {
+        Position firstBacillusPos = new Position(3, 5);
+        bacteriaList.add(new Bacillus(bacteriaList.size() + 1, firstBacillusPos));
+        Position firstCoccusPos = new Position(2, 4);
+        bacteriaList.add(new Coccus(bacteriaList.size() + 1, firstCoccusPos));
+    }
+
+    public void setWidth(int width) {
+        this.width = width;
+    }
+
+    public int getWidth() {
+        return width;
+    }
+
+    public static void setTimePulse() {
+        timePulse = 0;
+    }
+
+    public void addBacteria(Bacteria bacteria) {
+        bacteriaList.add(bacteria);
+    }
+
+    public List<Coccus> getCoccuses() {
+        List<Coccus> coccuses = new ArrayList<>();
+        for (Bacteria bacteria : bacteriaList) {
+            if (bacteria.getType() == BacteriaType.COCCUS) {
+                coccuses.add((Coccus) bacteria);
+            }
+        }
+        return coccuses;
+    }
+}
