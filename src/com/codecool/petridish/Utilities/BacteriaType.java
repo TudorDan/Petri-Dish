8bf3a701b74456cea3d3d1e06dbba60f324fd52c
TudorDan <tudor.ist@gmail.com>
2020-08-24 19:50:34 +0300

    Refactor BacteriaType enum

--- src/com/codecool/petridish/Utilities/BacteriaType.java
+++ src/com/codecool/petridish/Utilities/BacteriaType.java
@@ -1,7 +1,17 @@
 package com.codecool.petridish.Utilities;
 
 public enum BacteriaType {
-    BACILLUS,
-    COCCUS,
-    SPIRILLUM
+    BACILLUS("B"),
+    COCCUS("C"),
+    SPIRILLUM("S");
+
+    private final String name;
+
+    private BacteriaType(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
 }
